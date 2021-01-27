27e30305268304f1267bd0001ab4ada5c2e5c0e9
kyung vin <pennyyi97@gmail.com>
2021-01-27 16:24:48 +0900

    Server Spring Boot Employee 백엔드 구현

---
+++ server/angularapi/src/main/java/com/penny/angularpi/angularapi/AngularapiApplication.java
@@ -0,0 +1,13 @@
+package com.penny.angularpi.angularapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularapiApplication.class, args);
+	}
+
+}
